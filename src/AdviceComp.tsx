f745378fe1672969346c98829d429fe5df7a51db
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-04-15 20:12:00 -0400

    UserS-3 completed

--- src/AdviceComp.tsx
+++ src/AdviceComp.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import "./AdviceComp.css";
+
+type Data = {
+  slip: {
+    advice: string;
+  };
+};
 const AdviceComp = () => {
+  const [advice, setAdvice] = useState("");
+
+  const fetchData = async (): Promise<Data> => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    return data;
+  };
+
+  useEffect(() => {
+    const changeText = async () => {
+      const adviceInfo = await fetchData();
+      setAdvice(adviceInfo.slip.advice);
+    };
+
+    changeText();
+  }, []);
   return (
     <section className="container">
-      <section className="card">Hello from the advice comp</section>
+      <section className="card">{advice}</section>
     </section>
   );
 };
